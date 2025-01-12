6db29613e96d037c52d9be7021bd5e440921e2b4
Hernán Nesprías <nan4k7@yahoo.com.ar>
2025-01-12 18:32:28 -0300

    Optimizacion de consumo

--- esp32_antiparallel_led.ino
+++ esp32_antiparallel_led.ino
@@ -1,7 +1,12 @@
 #include <WiFi.h>
+#include "esp_wifi.h"
+#include "esp_pm.h"
+#include "esp_sleep.h"
 #include <NTPClient.h>
+#include <AsyncTCP.h>
 #include <WiFiUdp.h>
 #include <ESPAsyncWebServer.h>
+#include <ElegantOTA.h>
 #include <Preferences.h>
 
 
@@ -9,19 +14,26 @@
 #define LED_A 2
 #define LED_B 3
 #define LED_ALT 0
-#define WIFI_SWITCH_PIN 1  // GPIO para el interruptor Wi-Fi
-#define FORCEON_SWITCH_PIN -1 //GPIO para el interruptor de encendido forzado
+#define WIFI_SWITCH_PIN 1      // GPIO para el interruptor Wi-Fi
+#define FORCEON_SWITCH_PIN -1  //GPIO para el interruptor de encendido forzado
+#define POTPOWER_PIN -1        // GPIO para el potenciómetro (-1 para desactivar)
+#define DEVNAME "esp32-led-candlelight"
+
+#define WIFI_SSID "Fibergarch 2.4"
+#define WIFI_PASS "Hipolito480"
+
 
 unsigned long currentMillis;
-
-// Timing Variables
+unsigned long otaProgMillis = 0;
 unsigned long previousMillis = 0;
+
 const long interval = 10;  // 150Hz = 6.67ms per cycle
 bool cycle = false;
 bool running = false;
 bool firstRun = false;
 bool altLedFlag = false;
 bool forceOnFlag = false;
+bool potPinFlag = false;
 bool forceOn = false;
 int currentHour = -1;
 int currentMin = -1;
@@ -31,22 +43,18 @@ int endHourDef = 4;
 int pwmPowerDef = 5;
 
 // Wi-Fi
-const char* ssid = "Fibergarch 2.4";
-const char* password = "Hipolito480";
-const char* hostname = "esp32-led-candlelight";
 bool wifiAlwaysOn = false;
 
-//NTP
-unsigned long lastNTPUpdate = 0;                 // Almacena el tiempo de la última actualización NTP
-const unsigned long ntpUpdateInterval = 21600000; // 6 horas en milisegundos (6 * 3600 seg * 1000 ms)
+// NTP
+unsigned long lastNTPUpdate = 0;                   // Almacena el tiempo de la última actualización NTP
+const unsigned long ntpUpdateInterval = 21600000;  // 6 horas en milisegundos
 const unsigned long ntpUpdateIntervalRetry = 30000;
 
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", (-3 * 3600), (ntpUpdateInterval - 5000));  // UTC, syncs every (ntpUpdateInterval - 5s)
+NTPClient timeClient(ntpUDP, "pool.ntp.org", (-3 * 3600), (ntpUpdateInterval - 5000));
 bool timeClientEnabled = false;
 
-
-// Preferences for Storing Configuration
+// Preferences
 Preferences preferences;
 int startHour;
 int endHour;
@@ -55,6 +63,7 @@ int pwmPower;
 // Web Server
 AsyncWebServer server(80);
 bool serverRunning = false;  // Estado del servidor HTTP
+bool otaRunning = false;
 
 // Wi-Fi Control Functions
 void connectWiFi() {
@@ -64,19 +73,32 @@ void connectWiFi() {
   setCpuFrequencyMhz(80);
 
   //WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, INADDR_NONE);
-  WiFi.setHostname(hostname);
+  WiFi.setHostname(DEVNAME);
   WiFi.mode(WIFI_STA);
   WiFi.setSleep(true);  // Activa el modo de bajo consumo en Wi-Fi
 
+  const int LISTEN_INTERVAL = 10;
+
+  wifi_config_t wifi_config = {
+    .sta = {
+      .ssid = WIFI_SSID,
+      .password = WIFI_PASS,
+      .listen_interval = LISTEN_INTERVAL,
+    },
+  };
+
+  esp_wifi_set_ps(WIFI_PS_MAX_MODEM);
+  esp_wifi_set_max_tx_power(40);
+  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
 
   if (WiFi.status() != WL_CONNECTED) {
     Serial.print("WiFi conectando");
-    WiFi.begin(ssid, password);
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
 
     while (WiFi.status() != WL_CONNECTED) {
       delay(1000);
       Serial.print(".");
-      
+
       retryCount++;
 
       if (retryCount > maxRetries) {
@@ -122,6 +144,47 @@ void endTimeClient() {
   }
 }
 
+void startOTA() {
+  if (!otaRunning) {
+    ElegantOTA.begin(&server);  // Start ElegantOTA
+    ElegantOTA.onStart(onOTAStart);
+    ElegantOTA.onProgress(onOTAProgress);
+    ElegantOTA.onEnd(onOTAEnd);
+
+    otaRunning = true;
+
+    Serial.println("Servidor OTA iniciado");
+  }
+}
+
+void stopOTA() {
+  if (otaRunning) {
+    otaRunning = false;
+
+    Serial.println("Servidor OTA detenido");
+  }
+}
+
+void onOTAStart() {
+  Serial.println("OTA update iniciado!");
+}
+
+void onOTAProgress(size_t current, size_t final) {
+  if (millis() - otaProgMillis > 1000) {
+    otaProgMillis = millis();
+    Serial.printf("OTA Progress Current: %u bytes, Final: %u bytes\n", current, final);
+  }
+}
+
+void onOTAEnd(bool success) {
+  // Log when OTA has finished
+  if (success) {
+    Serial.println("OTA update finalizado successfully!");
+  } else {
+    Serial.println("Error durante el update OTA!");
+  }
+}
+
 void startServer() {
   if (!serverRunning) {
     server.begin();
@@ -145,7 +208,26 @@ void updateLocalTime() {
   currentHour = timeClient.getHours();
 }
 
+void updatePWMPowerFromPot() {
+  if (potPinFlag) {
+    int potValue = analogRead(POTPOWER_PIN) / 10;  // Leer valor (0-4095)
+
+    int pwmPowerTmp = map(potValue, 0, 409, 0, 100);  // Escalar a 0-100
+
+    const int threshold = 5;
+
+    if (abs(pwmPower - pwmPowerTmp) >= threshold) {
+      pwmPower = pwmPowerTmp;
+      preferences.putInt("pwmPower", pwmPower);
+
+      Serial.println("Potencia LED: " + String(pwmPower) + "%");
+    }
+  }
+}
+
 void runCycle() {
+  updatePWMPowerFromPot();
+
   if (currentMillis - previousMillis >= interval) {
     analogWrite(LED_A, cycle ? (pwmPower * 2.55) : LOW);
     analogWrite(LED_B, cycle ? LOW : (pwmPower * 2.55));
@@ -159,21 +241,20 @@ void setup() {
   Serial.begin(115200);
   setCpuFrequencyMhz(80);
 
-  if (LED_ALT >= 0)
-    altLedFlag = true;
-  if (FORCEON_SWITCH_PIN >= 0)
-    forceOnFlag = true;
+  if (LED_ALT >= 0) altLedFlag = true;
+  if (FORCEON_SWITCH_PIN >= 0) forceOnFlag = true;
+  if (POTPOWER_PIN >= 0) potPinFlag = true;
 
   pinMode(LED_A, OUTPUT);
   pinMode(LED_B, OUTPUT);
   pinMode(WIFI_SWITCH_PIN, INPUT_PULLUP);
-  
-  if (forceOnFlag)
-    pinMode(FORCEON_SWITCH_PIN, INPUT_PULLUP);
+
+  if (forceOnFlag) pinMode(FORCEON_SWITCH_PIN, INPUT_PULLUP);
+  if (potPinFlag) pinMode(POTPOWER_PIN, INPUT);
 
   analogWrite(LED_A, LOW);
   analogWrite(LED_B, LOW);
-  
+
   if (altLedFlag) {
     pinMode(LED_ALT, OUTPUT);
     digitalWrite(LED_ALT, LOW);
@@ -202,14 +283,20 @@ void setup() {
           End Hour: <input type="number" name="endHour" min="0" max="23" value=")rawliteral";
     html += String(endHour);
     html += R"rawliteral("><br>
-          Power: <input type="number" name="pwmPower" min="0" max="100" value=")rawliteral";
+          Power: <input type="number" name="pwmPower" min="0" max="100" )rawliteral";
+
+    // Usamos una variable para el atributo disabled
+    html += (potPinFlag ? R"rawliteral(disabled="disabled")rawliteral" : "");
+    html += R"rawliteral( value=")rawliteral";
     html += String(pwmPower);
-    html += R"rawliteral("><br>    
+    html += R"rawliteral("><br>
           <input type="submit" value="Save">
         </form>
       </body>
       </html>
     )rawliteral";
+
+
     request->send(200, "text/html", html);
   });
 
@@ -217,37 +304,37 @@ void setup() {
     if (request->hasParam("startHour", true) && request->hasParam("endHour", true)) {
       startHour = request->getParam("startHour", true)->value().toInt();
       endHour = request->getParam("endHour", true)->value().toInt();
-      pwmPower = request->getParam("pwmPower", true)->value().toInt();
 
       preferences.begin("led-config", false);
       preferences.putInt("startHour", startHour);
       preferences.putInt("endHour", endHour);
-      preferences.putInt("pwmPower", pwmPower);
+
+      if (!potPinFlag) {
+        pwmPower = request->getParam("pwmPower", true)->value().toInt();
+        preferences.putInt("pwmPower", pwmPower);
+      }
+
       preferences.end();
 
-        Serial.println("Configuración guardada");
+      Serial.println("Configuración guardada");
 
-        request->redirect("/");
-    }
-    else {
+      request->redirect("/");
+    } else {
       request->send(400, "text/plain", "Invalid Input");
     }
   });
-
   Serial.println("Setup completado");
 }
 
 void loop() {
   currentMillis = millis();
 
-  // Leer el estado del interruptor
   wifiAlwaysOn = digitalRead(WIFI_SWITCH_PIN) == HIGH;
-  
-  if (forceOnFlag)
-    forceOn = digitalRead(FORCEON_SWITCH_PIN) == HIGH;    
+  if (forceOnFlag) forceOn = digitalRead(FORCEON_SWITCH_PIN) == HIGH;
 
   if ((!wifiAlwaysOn && (currentMillis - lastNTPUpdate >= ntpUpdateInterval || lastNTPUpdate == 0)) || wifiAlwaysOn) {
     connectWiFi();
+    startOTA();
     startServer();
 
     initTimeClient();
@@ -261,21 +348,19 @@ void loop() {
     if (!wifiAlwaysOn) {
       disconnectWiFi();
       //endTimeClient();
+      stopOTA();
       stopServer();
     }
 
     lastNTPUpdate = currentMillis;  // Actualizar el tiempo de la última sincronización
-  }
-  else {
+  } else {
     disconnectWiFi();
     //endTimeClient();
+    stopOTA();
     stopServer();  // Asegurar que el servidor esté apagado si Wi-Fi está desconectado
   }
 
-  //Serial.println(currentHour);
-
   bool mismoDia = startHour <= endHour;
-
 
   if (!timeClient.isTimeSet()) {
     analogWrite(LED_A, LOW);
@@ -293,8 +378,7 @@ void loop() {
 
       lastNTPUpdate = currentMillis;
     }
-  }
-  else {
+  } else {
     updateLocalTime();
 
     if (forceOn || (mismoDia && (currentHour >= startHour && currentHour < endHour)) || (!mismoDia && (currentHour >= startHour || currentHour < endHour))) {
@@ -309,27 +393,28 @@ void loop() {
 
       if (altLedFlag) {
         digitalWrite(LED_ALT, HIGH);
-      }      
-    }
-    else {
-      analogWrite(LED_A, LOW);
-      analogWrite(LED_B, LOW);
-
-      if (altLedFlag) {
-        digitalWrite(LED_ALT, LOW);
       }
-
+    } else {
       if (running || !firstRun) {
         running = false;
         firstRun = true;
 
-        Serial.println("LED apagado fuera del rango de tiempo");
+        analogWrite(LED_A, LOW);
+        analogWrite(LED_B, LOW);
 
-        if (!wifiAlwaysOn) {
-          esp_sleep_enable_timer_wakeup(15 * 1000000);
-          esp_light_sleep_start(); 
+        if (altLedFlag) {
+          digitalWrite(LED_ALT, LOW);
         }
+
+        Serial.println("LED apagado fuera del rango de tiempo");
+      }
+
+      if (!wifiAlwaysOn) {
+        esp_sleep_enable_timer_wakeup(10 * 1000000);
+        esp_light_sleep_start();
       }
     }
   }
+
+  ElegantOTA.loop();
 }
